e9234e2017fb1f11d0236bdb5a9158448a6a4c80
Pavan Kumar <pavannayakiict@gmail.com>
2021-04-29 13:12:38 +0530

    Add project files.

---
+++ PractiseExamples/CustomerAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PractiseExamples
+{
+    public class CustomerAttribute :Attribute
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+
+        public CustomerAttribute(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+        public CustomerAttribute()
+        {
+            this.Id = -1;
+            this.Name = String.Empty;
+        }
+    }
+}
---
+++ PractiseExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace PractiseExamples
+{
+    [Customer(Id = 1249, Name = "Pavan")]
+    class Program
+    {
+
+        
+        public static void GetValues(Type t)
+        {
+            CustomerAttribute attr = (CustomerAttribute)Attribute.GetCustomAttribute(t, typeof(CustomerAttribute));
+
+            if (attr == null)
+            {
+                Console.WriteLine("The Attribute Was Not Found");
+            }
+            else
+            {
+                Console.WriteLine(attr.Id);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Type T = Type.GetType("PractiseExamples.CustomerAttribute");
+            Console.WriteLine(typeof(Program));
+            GetValues(typeof(Program));
+
+            
+        }
+
+    }
+}
